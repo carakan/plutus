b9b6d693ac669cab4eb4b77038d265a7e79d2d43
Carlos Ramos <carakan@gmail.com>
2019-11-29 17:26:23 -0400

    fix

--- app/controllers/plutus/application_controller.rb
+++ app/controllers/plutus/application_controller.rb
@@ -1,6 +1,14 @@
 module Plutus
   class ApplicationController < ActionController::Base
     before_action :authenticate_admin_user!
-    before_action :authorize_resources  
+    before_action :authorize_resources
+    
+    protected
+
+    def authorize_resources
+      if !params[:controller].include?('devise') && !current_admin_user.admin?
+        raise CanCan::AccessDenied
+      end
+    end  
   end
 end
